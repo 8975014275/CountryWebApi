2a81ee7aefd90b5291e4b41c62822f5e76febb63
8975014275 <avgiri1998@gmail.com>
2022-12-16 09:16:16 +0530

    Add project files.

---
+++ CountryWebApi/Model/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CountryWebApi.Model
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<CountryItem> CountryItems { get; set; } = null!;
+    }
+}
---
+++ CountryWebApi/Model/CountryItem.cs
@@ -0,0 +1,10 @@
+﻿namespace CountryWebApi.Model
+{
+    public class CountryItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? State { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
